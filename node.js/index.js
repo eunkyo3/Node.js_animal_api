dcbcdbf1751514619b91849b03eaa58fa1c02009
JungEunKyo <jyk8929@naver.com>
2023-07-27 03:41:55 +0900

    animal api

--- node.js/index.js
+++ node.js/index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/sound/:name', (req, res) => {
+  const { name } = req.params
+  
+  if(name == "dog"){
+    res.json({'sound' : '멍멍'})
+  } else if(name == "cat"){
+    res.json({'sound' : '야옹'})
+  } else if(name == 'pig'){
+    res.json({'sound' : '꿀꿀'})
+  } 
+  else if(name == '민준'){
+    res.json({'sound' : '꿀꿀'})
+  }
+  else {
+    res.json({'sound' : '알수없음'})
+  }
 })
 
 app.listen(3000)
